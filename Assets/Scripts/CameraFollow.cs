13e6a6bb384a3e17dec07a4d1bdf335e1169ccb3
Juan Luis Cantón Garay <juanluiscantongaray@iMac-de-Juan.local>
2020-12-08 13:31:16 +0100

    add playerController and cameraFollow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Vector3 cameraOffset = new Vector3(0f, 1.3f, -3f);
+    private Transform target;
+
+     void Start() 
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+    void LateUpdate() 
+    {
+        transform.position = target.TransformPoint(cameraOffset);
+        transform.LookAt(target);
+    }
+    
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed;
+    public float rotateSpeed;
 
-    // Update is called once per frame
+    private float hInput, vInput;
+
     void Update()
     {
-        
+        InitAxis();
+        TranslatePlayer();
+        RotatePlayer();
     }
+
+    void InitAxis()
+    {
+        hInput = Input.GetAxis("Horizontal")*rotateSpeed;
+        vInput = Input.GetAxis("Vertical")*moveSpeed;
+    }
+
+    void TranslatePlayer()
+    {
+        transform.Translate(vInput * Time.deltaTime * Vector3.forward);
+    }
+
+    void RotatePlayer()
+    {
+        transform.Rotate(hInput* Time.deltaTime * Vector3.up);
+    }
+
 }
