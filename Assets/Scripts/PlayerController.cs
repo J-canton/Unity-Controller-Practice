e4e317dd8b65503daea2a34464315dae521fa6bf
Juan Luis Cantón Garay <juanluiscantongaray@iMac-de-Juan.local>
2020-12-08 18:55:49 +0100

    player can jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,14 +6,30 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float rotateSpeed;
+    public float jumSpeed;
 
+    public float distanceToGround = 0.1f;
+    public LayerMask groundLayer;
     private float hInput, vInput;
+
+    private Rigidbody _rb;
+    private CapsuleCollider _col;
+
+    void Start() 
+    {
+        _rb = GetComponent<Rigidbody>();
+        _col  = GetComponent<CapsuleCollider>();
+    }
 
     void Update()
     {
         InitAxis();
-        TranslatePlayer();
-        RotatePlayer();
+    }
+
+    void FixedUpdate() 
+    {
+        PhysicRotation();   
+        Jump();
     }
 
     void InitAxis()
@@ -22,6 +38,7 @@ public class PlayerController : MonoBehaviour
         vInput = Input.GetAxis("Vertical")*moveSpeed;
     }
 
+    // MOVE WITHOUT PHYSIC
     void TranslatePlayer()
     {
         transform.Translate(vInput * Time.deltaTime * Vector3.forward);
@@ -30,6 +47,30 @@ public class PlayerController : MonoBehaviour
     void RotatePlayer()
     {
         transform.Rotate(hInput* Time.deltaTime * Vector3.up);
+    }
+
+    //MOVE WITH PHYSIC
+    void PhysicRotation(){
+        Vector3 rotation = Vector3.up * hInput;
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+
+        _rb.MovePosition(transform.position + transform.forward * vInput * Time.fixedDeltaTime);
+        _rb.MoveRotation(_rb.rotation * angleRot);
+    }
+
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)&& IsOnTheGround())
+        {
+            _rb.AddForce(Vector3.up * jumSpeed, ForceMode.Impulse);
+        }
+    }
+
+    bool IsOnTheGround()
+    {
+    Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+    bool onTheGround = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, distanceToGround, groundLayer, QueryTriggerInteraction.Ignore);
+    return onTheGround;
     }
 
 }
