cb4b145a880dc00eae2fa2e512cae01131376996
Juan Luis Cantón Garay <juanluiscantongaray@iMac-de-Juan.local>
2020-12-09 09:36:18 +0100

    add gameManeger

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int _playerHP = 100;
+    public int PlayerHp {
+        get
+        {
+            return _playerHP;
+        } 
+        set
+        {
+            _playerHP = value;
+            if(value - _playerHP < 0)
+            {
+                _playerHP = 0;
+            }
+        }
+    }
+}
